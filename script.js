194206567a4c87b30d2a35b4a2920e69b0e29ce7
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2025-04-09 00:04:45 +0200

    Added a page for my profile and linked the button

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+
+//on click event listeners for the buttons
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.getElementById("search");
   
     if (button) {
       button.addEventListener("click", function () {
         window.location.href = "results_page.html";
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("lucky");
+  
+    if (button) {
+      button.addEventListener("click", function () {
+        window.location.href = "my_profile.html";
       });
     }
   });
